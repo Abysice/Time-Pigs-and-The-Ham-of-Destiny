4b5893634550d83b749b405dfc42960a8be6aab3
Bryan <ramoulb@gmail.com>
2015-12-16 16:57:03 -0500

    Time slider with lerp
    created a slider with a lerp function

---
+++ Assets/Development/Scripts/TRSlider.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TRSlider : MonoBehaviour {
+
+    public Slider TRScroll;
+    private Image img;
+    private const float LERP_MULTIPLIER = 0.3f;
+
+	// Use this for initialization
+	void Start () {
+        img = gameObject.GetComponent<Image>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        img.fillAmount = Mathf.Lerp(img.fillAmount, TRScroll.value, LERP_MULTIPLIER); 
+	}
+}
