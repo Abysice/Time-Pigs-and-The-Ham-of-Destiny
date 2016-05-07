6e0fa812e8efcdebc75fea8feb6eec45d29a0868
Adam Bysice <adambysice@gmail.com>
2016-05-07 13:23:41 -0400

    prototype

--- Assets/Development/Scripts/PawnMaster/PlayerController.cs
+++ Assets/Development/Scripts/PawnMaster/PlayerController.cs
@@ -8,9 +8,9 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	#region Public Variables
-	private float MAGIC_TIMER = 0.1f;
 
-	public const float MOVE_SPEED = 0.05f;
+
+	private const float MOVE_SPEED = 0.05f;
 	#endregion
 
 	#region Protected Variables
@@ -32,7 +32,7 @@ public class PlayerController : MonoBehaviour {
 	{
 		m_inp = Managers.GetInstance().GetInputManager();
 		m_cmanager = Managers.GetInstance().GetCommandManager();
-		m_moveTimer = MAGIC_TIMER;
+		m_moveTimer = m_cmanager.GetTimer();
 	}
 	//runs every frame
 	public void Update()
@@ -40,33 +40,29 @@ public class PlayerController : MonoBehaviour {
 
 		if (Managers.GetInstance().GetGameStateManager().CurrentState == Enums.GameStateNames.GS_03_INPLAY)
 		{
-			if (m_moveTimer > 0.0f) // do nothing this frame
+			if (m_cmanager.GetTimer() > 0.0f) // do nothing this frame
 			{
-				m_moveTimer -= Time.deltaTime;
 				return;
 			}
-			else if (m_moveTimer < 0.0f)
-			{
-				m_moveTimer = MAGIC_TIMER;
-			}
+
 
 			//PlaceHolder Code
 			Vector3 m_inputVec = Vector3.zero;
 			if (Input.GetKey(KeyCode.W))
 			{
-				m_inputVec = (Vector3.up * MOVE_SPEED);
+				m_inputVec += (Vector3.up * MOVE_SPEED);
 			}
 			else if (Input.GetKey(KeyCode.S))
 			{
-				m_inputVec = (Vector3.down * MOVE_SPEED);
+				m_inputVec += (Vector3.down * MOVE_SPEED);
 			}
 			if (Input.GetKey(KeyCode.D))
 			{
-				m_inputVec = (Vector3.right * MOVE_SPEED);
+				m_inputVec += (Vector3.right * MOVE_SPEED);
 			}
 			if (Input.GetKey(KeyCode.A))
 			{
-				m_inputVec = (Vector3.left * MOVE_SPEED);
+				m_inputVec += (Vector3.left * MOVE_SPEED);
 			}
 			m_cmanager.AddMoveCommand(gameObject, gameObject.transform.position + m_inputVec);
 			
